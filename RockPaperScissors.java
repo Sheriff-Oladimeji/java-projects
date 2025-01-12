01305c3328abc3ff942b94365854462a2cca5d71
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2025-01-12 06:32:38 +0100

    feat: add check for invalid value

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -6,30 +6,34 @@ public class RockPaperScissors {
         System.err.println("Welcome to Rock-Paper-Scissors! ");
         System.out.println("Enter your choice: rock, paper, or scissors: ");
         String userChoice = myScanner.nextLine();
-       
+while (!userChoice.equalsIgnoreCase("rock") && !userChoice.equalsIgnoreCase("paper") && !userChoice.equalsIgnoreCase("scissors")) {
+        System.out.println("Invalid choice. Please enter rock, paper, or scissors: ");
+        userChoice = myScanner.nextLine();
+    
+}
         String[] choices = { "rock", "paper", "scissors" };
         int len = choices.length;
         int randomIndex = (int) (Math.random() * len);
         String computerChoice = choices[randomIndex];
         System.out.println("You chose: " + userChoice);
         System.out.println("Computer chose: " + computerChoice);
-        if (userChoice.toLowerCase() == computerChoice.toLowerCase()) {
-            System.out.println("It's a tie!");
+        if (userChoice.equalsIgnoreCase(computerChoice)) {
+        System.out.println("It's a tie!");
         }
-        else if (userChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors") {
-            System.out.println("You win!");
+        else if (userChoice.equalsIgnoreCase("rock") &&  computerChoice.equalsIgnoreCase("scissors")) {
+        System.out.println("You win!");
         }
-        else if (userChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper") {
-            System.out.println("You win!");
+        else if (userChoice.equalsIgnoreCase("scissors") && computerChoice.equalsIgnoreCase("paper")) {
+        System.out.println("You win!");
         }
-        else if (userChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock") {
-            System.out.println("You win!");
+        else if (userChoice.equalsIgnoreCase("paper") && computerChoice.equalsIgnoreCase("rock")) {
+        System.out.println("You win!");
         }
         else {
-            System.out.println("Computer wins!");
+        System.out.println("Computer wins!");
 
         }
-      
+    
         myScanner.close();
 
     }
